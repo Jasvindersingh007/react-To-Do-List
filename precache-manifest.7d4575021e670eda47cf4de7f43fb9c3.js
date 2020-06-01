d6ba0966e83abbb19f982c43ca108d90b7a415ee
jasvindersingh007 <jasvinder.lelanee@gmail.com>
2020-06-01 13:20:23 +0530

    Updates

---
+++ precache-manifest.7d4575021e670eda47cf4de7f43fb9c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e486a12a5d284de2b617b0312910d907",
+    "url": "/react-To-Do-List/index.html"
+  },
+  {
+    "revision": "90789f772a29f4e83b41",
+    "url": "/react-To-Do-List/static/css/main.3a05686e.chunk.css"
+  },
+  {
+    "revision": "b909f3c1fbb367c6f1f5",
+    "url": "/react-To-Do-List/static/js/2.f973de4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-To-Do-List/static/js/2.f973de4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90789f772a29f4e83b41",
+    "url": "/react-To-Do-List/static/js/main.95aac658.chunk.js"
+  },
+  {
+    "revision": "c969490831712eff01ee",
+    "url": "/react-To-Do-List/static/js/runtime-main.7e6f8f2f.js"
+  }
+]);
